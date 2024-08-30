e4bf988d3bd5ea3c615437b2c836b37b107a3bae
Sabbirahmed0007 <>
2024-08-30 09:32:42 +0600

    added description of element

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 
 
 // Method getElementById()
-let msg = document.getElementById("message");
+let msg = document.getElementById("message");  // Returns element based on id name
 console.log(msg);
 
 
 
 //Method getElementByName 
-let btn = document.getElementsByName("language")
+let btn = document.getElementsByName("language") //Returns input elements based on name
 console.log(btn);
 
 
 // Method getElementsByTagname
-let heading = document.getElementsByTagName('h1')
+let heading = document.getElementsByTagName('h1')  // Returns element based on Tag Name
 console.log(heading);
 
 
 // Method getElementByClassName
 
-let msgs = document.getElementsByClassName("messages");
+let msgs = document.getElementsByClassName("messages"); // Returns element based on class name
 console.log(msgs);
 
 
